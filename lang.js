471d5a9ebbd2ed2b1a0ef86e951f98b0e2e68df2
Valkovid <mickael.jacob@outlook.com>
2025-06-01 15:04:26 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,34 @@
+const LANG = {
+  fr: {
+    title: "Phantome Investigation - par Valkovid",
+    reset: "🔁 Réinitialiser les filtres",
+    grid: "🔧 Grille : Mouvement détecté :",
+    yes: "OUI",
+    no: "NON",
+    clues: "🔎 Indices trouvés :",
+    matchCount: "Fantômes correspondants :",
+    ghost: {
+      voice: "🗣️ Voix",
+      grid: "👁️ Mouvement Grille",
+      danger: "☠️ Dangerosité",
+      speed: "🚶 Déplacement",
+      clues: "🔍 Indices"
+    }
+  },
+  en: {
+    title: "Phantome Investigation - by Valkovid",
+    reset: "🔁 Reset filters",
+    grid: "🔧 Grid : Movement detected:",
+    yes: "YES",
+    no: "NO",
+    clues: "🔎 Clues found:",
+    matchCount: "Matching ghosts:",
+    ghost: {
+      voice: "🗣️ Voice",
+      grid: "👁️ Grid movement",
+      danger: "☠️ Danger level",
+      speed: "🚶 Speed",
+      clues: "🔍 Clues"
+    }
+  }
+};
